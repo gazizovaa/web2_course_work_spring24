c0e79de798e0330c90ade9b2bf1155ebc12cafb3
gazizovaa <gazizova12047@ada.edu.az>
2024-02-28 00:42:46 +0400

    Created CRUD app for the lib system

---
+++ 273/crud-library-spring-app/src/test/java/az/edu/ada/wm2/crudlibraryspringapp/CrudLibrarySpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.crudlibraryspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLibrarySpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
