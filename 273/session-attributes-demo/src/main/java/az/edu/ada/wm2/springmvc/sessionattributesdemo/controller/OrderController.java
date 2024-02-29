04351243234b716c07200656379d6ad7df604d2d
gazizovaa <gazizova12047@ada.edu.az>
2024-03-01 00:25:08 +0400

    Added products and orders controllers

---
+++ 273/session-attributes-demo/src/main/java/az/edu/ada/wm2/springmvc/sessionattributesdemo/controller/OrderController.java
@@ -0,0 +1,27 @@
+package az.edu.ada.wm2.springmvc.sessionattributesdemo.controller;
+
+import az.edu.ada.wm2.springmvc.sessionattributesdemo.model.Order;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.DefaultSessionAttributeStore;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.context.request.WebRequest;
+
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/")
+    public String listOrderDetails(@SessionAttribute Order order, Model model){
+        model.addAttribute("order", order);
+        return "orders/order_detail";
+    }
+
+    @PostMapping("/save")
+    public String saveOrder(@RequestParam String customerName, @SessionAttribute Order order){
+        order.setCustomerName(customerName);
+        System.out.println("Saving the order: " + order);
+        return "redirect:/products/list";
+    }
+}
