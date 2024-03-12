3e285f4db18f3691ec0b2471ee148dee2e52a7a0
gazizovaa <gazizova12047@ada.edu.az>
2024-03-13 01:19:03 +0400

    Created first jpa application

---
+++ 273/first-jpa-app/src/test/java/az/edu/ada/wm2/firstjpaapp/FirstJpaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.firstjpaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJpaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
