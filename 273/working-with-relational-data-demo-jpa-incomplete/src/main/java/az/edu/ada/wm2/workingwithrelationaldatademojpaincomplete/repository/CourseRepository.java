5e60aba5582f081ce210c94a0bba16594e62cfc1
Gulnara Azizova <78263828+gazizovaa@users.noreply.github.com>
2024-03-27 08:58:02 +0400

    Delete 273 directory

--- 273/working-with-relational-data-demo-jpa-incomplete/src/main/java/az/edu/ada/wm2/workingwithrelationaldatademojpaincomplete/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package az.edu.ada.wm2.workingwithrelationaldatademojpaincomplete.repository;
-
-import az.edu.ada.wm2.workingwithrelationaldatademojpaincomplete.model.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-
-}
